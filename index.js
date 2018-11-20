6e65fe5232ceca7595e290e1dda20aa333febcc5
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 16:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyPrependKitten(name){
  return kittens
 }
 
-function destructivelyRemoveLastKitten(name){
- kittens.pop(name)
+function destructivelyRemoveLastKitten(){
+ kittens.pop()
  return kittens
 }
